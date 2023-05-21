55619ecdac52d5475aa4e93b5de719c4f3fb5d21
atikazeynep <91607254+atikazeynep@users.noreply.github.com>
2023-05-22 01:26:55 +0300

    Revert "Supervisor endpoint and related classes are created. "
    This reverts commit 228edf9cd09689749a3aa1e333c73074dd9dcdd7.

--- Frontend/bilport-webapp/src/components/student/SupervisorInfo.jsx
+++ Frontend/bilport-webapp/src/components/student/SupervisorInfo.jsx
@@ -1,41 +1,9 @@
 import { Card, Container, Row, Col, Accordion, Form, Button } from "react-bootstrap";
-import { useRef, useState, useEffect, useLayoutEffect, version } from 'react';
-import useAuth from '../../hooks/useAuth';
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
-async function fetchSupervisor(axiosPrivate, auth, setSupervisor) {
-    try {
-        let x = auth.user.substring(2, 4).concat(auth.user.substring(0, 2),auth.user.substring(4));
-        const response = await axiosPrivate.get('/supervisors/'+x);
-        setSupervisor(response.data);
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-export default function SupervisorInfo() {
-
-    const { auth } = useAuth();
-
-    const [supervisor, setSupervisor] = useState([]);
-
-    const axiosPrivate = useAxiosPrivate();
-
-    useEffect(() => {
-        console.log(auth);
-        fetchSupervisor(axiosPrivate, auth, setSupervisor);
-    }
-        , []);
-
-    useEffect(() => {
-        console.log(supervisor);
-    }
-        , [supervisor]);
-
-
+export default function SupervisorInfo(){
     { /* IMPORTANT: There should be a "No supervisor info" display if the student hasn't entered a supervisor yet*/}
     return(
-        <div style={{ marginLeft: '250px', padding: '20px' }}>
+        <div style={{ marginLeft: '250px', padding: '20px' }}> 
             <h1 className = "bigPageTitle"> Supervisor Information </h1>
             <Card>
                 <Card.Body>
@@ -46,26 +14,26 @@ export default function SupervisorInfo() {
                             <hr/>
                             <Row>
                                 <Col lg = {2} style={{ color: "purple"}}>Full name: </Col>
-                            <Col lg={10}><div className="text-secondary">{supervisor.supervisorFullName}</div></Col>
+                                <Col lg = {10}><div className = "text-secondary">Super Visor</div></Col>
                             </Row>
                             <Row>
                                 <Col lg = {2} style={{ color: "purple"}}>Email: </Col>
-                                <Col lg = {10} ><div className = "text-secondary">{supervisor.supervisorEmail}</div></Col>
+                                <Col lg = {10} ><div className = "text-secondary">supervisor@company.com</div></Col>
                             </Row>
                             <br/>
                             <Card.Subtitle><b>About: </b></Card.Subtitle>
                             <hr/>
                             <Row>
                                 <Col lg = {2} style={{ color: "purple"}}>Working position: </Col>
-                                <Col lg = {10}><div className = "text-secondary">{supervisor.working_position}</div></Col>
+                                <Col lg = {10}><div className = "text-secondary">Senior Software Engineer</div></Col>
                             </Row>
                             <Row>
                                 <Col lg = {2} style={{ color: "purple"}}>Graduated from: </Col>
-                                <Col lg = {10}><div className = "text-secondary">{supervisor.graduation_university}</div></Col>
+                                <Col lg = {10}><div className = "text-secondary">ODTÜ</div></Col>
                             </Row>
                             <Row>
                                 <Col lg = {2} style={{ color: "purple"}}>University Major: </Col>
-                            <Col lg={10}><div className="text-secondary">{supervisor.university_major}</div></Col>
+                                <Col lg = {10}><div className = "text-secondary">Computer Science</div></Col>
                             </Row>
                     </Container>
                 </Card.Body>
@@ -77,117 +45,27 @@ export default function SupervisorInfo() {
     );
 }
 
-function ChangeSupervisorInfo() {
-    const { auth } = useAuth();
-
-    const [id, setID] = useState({});
-    const [supervisorFullName, setSupervisorFullName] = useState({});
-    const [supervisorEmail, setSupervisorEmail] = useState({});
-    const [working_position, setWorkingPosition] = useState({});
-    const [graduation_university, setGraduationUniversity] = useState({});
-    const [university_major, setUniversityMajor] = useState({});
-    const [supervisor, setSupervisor] = useState({});
-    //id =auth.user.substring(2, 4)+ auth.user.substring(0, 2)+ auth.user.substring(4);
-
-    const axiosPrivate = useAxiosPrivate();
-
-    //const [file, setFile] = useState();
-
-    const [showAlert, setShowAlert] = useState(false);
-    const [showSuccessfulAlert, setShowSuccessfulAlert] = useState(false);
-
-
-
-    const handleSubmitSupervisorInfo = () => {
-        if (!supervisorFullName || !supervisorEmail || !working_position || !graduation_university || !university_major){
-            setShowAlert(true);
-            return;
-        }
-        const submitSupervisor = async () => {
-            let x = auth.user.substring(2, 4).concat(auth.user.substring(0, 2),auth.user.substring(4));
-            try {
-                const response = await axiosPrivate.post(('/supervisors/'+x),
-                    {
-                        id: auth.user.substring(2, 4) + auth.user.substring(0, 2) + auth.user.substring(4),
-                        supervisorFullName: supervisorFullName,
-                        supervisorEmail: supervisorEmail,
-                        working_position: working_position,
-                        graduation_university: graduation_university,
-                        university_major: university_major,
-                        assignedStudentID: auth.user
-                    });
-
-
-            } catch (err) {
-                console.error(err);
-            }
-        };
-
-
-/*
-        const formData = new FormData();
-        formData.append('supervisor', supervisor)*/
-
-/*
-        const uploadSupervisorInfo = async () => {
-            try {
-                const response = await axiosPrivate.post('/supervisors/' + "x", formData,
-                    {
-                        headers: {"Content-Type": "application/json"},
-                    }
-                    ).then((response) => {console.log("\nCCCCC\n")});
-
-
-            } catch (err) {
-                console.error(err);
-            }
-        };*/
-
-        if (supervisorFullName != undefined) {
-            submitSupervisor();
-            //uploadSupervisorInfo();
-            console.log(id);
-            setShowSuccessfulAlert(true);
-            //document.getElementById("formReportName").value = "";
-        }
-
-    }
-
-
+function ChangeSupervisorInfo(){
     return(
     <div>
         <Accordion>
             <Accordion.Item eventKey="0">
                 <Accordion.Header>Enter/Edit Supervisor Information</Accordion.Header>
-                <Accordion.Body>
+                <Accordion.Body>                   
                     <Form.Group className="mb-3" controlId="changeSupervisorInfo">
                         <Form.Label>Full name:</Form.Label>
-                            <Form.Control type="text" onChange={ (e) => setSupervisorFullName(e.target.value)}  placeholder="Enter the full name of your supervisor" />
+                        <Form.Control type="text" placeholder="Enter the full name of your supervisor" />
                         <Form.Label>Email:</Form.Label>
-                        <Form.Control type="text" onChange={ (e) => setSupervisorEmail(e.target.value)} placeholder="Enter the mail address of your supervisor" />
+                        <Form.Control type="text" placeholder="Enter the mail address of your supervisor" />
                         <Form.Label>Working Position:</Form.Label>
-                        <Form.Control type="text" onChange={ (e) => setWorkingPosition(e.target.value)} placeholder="Enter the working position of your supervisor (e.g. senior software engineer)" />
+                        <Form.Control type="text" placeholder="Enter the working position of your supervisor (e.g. senior software engineer)" />
                         <Form.Label>Graduated From:</Form.Label>
-                        <Form.Control type="text" onChange={ (e) => setGraduationUniversity(e.target.value)} placeholder="Enter the university that your supervisor has graduated from" />
+                        <Form.Control type="text" placeholder="Enter the university that your supervisor has graduated from" />
                         <Form.Label>University Major:</Form.Label>
-                        <Form.Control type="text" onChange={ (e) => setUniversityMajor(e.target.value)} placeholder="Enter the graduation major of your supervisor (e.g. Computer Science)" />
+                        <Form.Control type="text" placeholder="Enter the graduation major of your supervisor (e.g. Computer Science)" />
                     </Form.Group>
-                        <Button variant="outline-primary" onClick={handleSubmitSupervisorInfo}> Submit</Button>
-              {/*  <div>
-                        <br/>
-                        { showAlert &&
-                        <Alert variant='warning' dismissible onClose={handleAlertDismiss}>
-                            Please enter both a file name and choose a file before submitting your report!
-                        </Alert> }
-                    </div>
-                    <div>
-                        <br/>
-                        { showSuccessfulAlert &&
-                        <Alert variant='success' dismissible onClose={() => {setShowSuccessfulAlert(false)}}>
-                            Report uploaded successfully.
-                        </Alert> }
-                    </div>*/}
-                    </Accordion.Body>
+                    <Button variant="outline-primary"> Submit</Button>
+                </Accordion.Body>
             </Accordion.Item>
         </Accordion>
     </div>
