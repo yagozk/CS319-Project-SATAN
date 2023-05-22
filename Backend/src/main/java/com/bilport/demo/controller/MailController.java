b60c58a20e1708ee1aa8803bf56102d1e23cf8e4
Ege Cenberci <egecenberci@gmail.com>
2023-05-22 21:37:22 +0300

    MailController added
    MailController class and the relevant dependencies added

---
+++ Backend/src/main/java/com/bilport/demo/controller/MailController.java
@@ -0,0 +1,32 @@
+package com.bilport.demo.controller;
+
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+public class MailController {
+    
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+     
+}
