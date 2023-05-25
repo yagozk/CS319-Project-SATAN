1cdf7c5354780638555a37c8e7b2cb6395a3aac5
Atika <zeynep.evmez@ug.bilkent.edu.tr>
2023-05-25 23:35:12 +0300

    A trial to get the student list from "students" collection in mongodb

--- Frontend/bilport-webapp/src/components/adminComponents/AdminManagement.jsx
+++ Frontend/bilport-webapp/src/components/adminComponents/AdminManagement.jsx
@@ -1,10 +1,80 @@
 import { Tab, Tabs } from "react-bootstrap";
 import AdminStudents from "./AdminStudents";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AdminEvaluators from "./AdminEvaluators";
+import useAuth from '../../hooks/useAuth';
+import axios from "axios";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
-export default function AdminManagement(){
+async function fetchUserAdmin(axiosInstance, auth, setAdmin) {
+    try {
+        const response = await axiosInstance.get(`/admins/${auth.user}`);
+        setAdmin(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
+async function fetchAllStudents(axiosInstance, setStudents) {
+    try {
+        const response = await axiosInstance.get(`/../students`);
+        setStudents(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function fetchAllEvaluators(axiosInstance, setEvaluator) {
+    try {
+        const response = await axiosInstance.all(`/../evaluators`);
+        setEvaluator(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default function AdminManagement() {
+const { auth } = useAuth();
+    const [admin, setAdmin] = useState([]);
+    const axiosPrivate = useAxiosPrivate();
+
+    useEffect(() => { fetchUserAdmin(axiosPrivate, auth, setAdmin); }, []);
+    console.log(admin);
+
+    useEffect(() => {
+        console.log(admin);
+    }, [admin]);
+
+  const [students, setStudents] = useState([]);
+    useEffect(() => { fetchAllStudents(axiosPrivate, setStudents); }, []);
+
+/*
+  useEffect(() => {
+    // Fetch student data from MongoDB collection
+    axiosPrivate.get(`/../students`)
+      .then(response => {
+        setStudents(response.data);
+      })
+      .catch(error => {
+        console.error(`Error fetching student data:`, error);
+      });
+  }, []);*/
+
+  const [evaluators, setEvaluators] = useState([]);
+  useEffect(() => { fetchAllEvaluators(axiosPrivate, setEvaluators); }, []);
+
+/*
+    useEffect(() => {
+    // Fetch student data from MongoDB collection
+    axiosPrivate.get(`/../evaluators`)
+      .then(response => {
+        setEvaluators(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching evaluator data:", error);
+      });
+  }, []);*/
+    /*
     // Need an API call in this endpoint for list of all students and list of all evaluators.
     const [students] = useState([
         {id: 220000, evaluatorID: 1, course: "CS299", name: "Osman Osmanovic", last_submission_date: "2001-01-01", status: "Assigned to course", email: "temp@gmail.com"},
@@ -21,9 +91,9 @@ export default function AdminManagement(){
         {id: 2, name: "Halil Altay Güvenir", email: "altay.guvenir@cs.bilkent.edu.tr", student_limit: 30, num_of_students: 18},
         {id: 3, name: "Fazlı Can", email: "fazli.can@cs.bilkent.edu.tr", student_limit: 40, num_of_students: 16},
         {id: 4, name: "Selim Aksoy", email: "selim.aksoy@cs.bilkent.edu.tr", student_limit: 30, num_of_students: 27}
-    ]);
+    ]);*/
 
-    return ( 
+    return (
         <div style={{ marginLeft: '250px', padding: '20px' }}>
             <Tabs defaultActiveKey="adminStudents" className="mb-3" fill>
                 <Tab eventKey="adminStudents" title = "Students" tabClassName='coloredTab'>
