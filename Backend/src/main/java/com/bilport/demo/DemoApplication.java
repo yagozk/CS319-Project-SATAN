0042558ab34afa4e75311fc788abab178768fd08
Ege Cenberci <egecenberci@gmail.com>
2023-05-26 19:58:21 +0300

    MailController updated
    now able to send mails, event to be tied to user creation

--- Backend/src/main/java/com/bilport/demo/DemoApplication.java
+++ Backend/src/main/java/com/bilport/demo/DemoApplication.java
@@ -1,12 +1,31 @@
 package com.bilport.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
+import com.bilport.demo.controller.MailController;
+import org.springframework.context.event.EventListener;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Autowired
+	private  MailController mailer;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		
 	}
+	/*
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail(){
+		mailer.sendEmail("egecenberci@gmail.com","Test subject","Test body");
+		
+	}
+	*/
 }
--- Backend/src/main/java/com/bilport/demo/controller/MailController.java
+++ Backend/src/main/java/com/bilport/demo/controller/MailController.java
@@ -5,12 +5,18 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MailController {
-    
+   
     @Autowired
     private JavaMailSender mailSender;
 
@@ -24,9 +30,28 @@ public class MailController {
             helper.setText(text, true);
 
             mailSender.send(message);
+            System.out.println("Mail sent hopefully");
         } catch (MessagingException e) {
             e.printStackTrace();
         }
     }
-     
+
+    /*
+    public void sendMail(){
+        MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
+            public void prepare(MimeMessage mimeMessage) throws Exception{
+                MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                message.setFrom("test@mail.com");
+                message.setTo("egecenberci@gmail.com");
+                message.setSubject("Mail subject");
+                message.setText("some text <img src='cid:logo'>", true);
+                //message.addInline("logo", new ClassPathResource("img/logo.gif"));
+                //message.addAttachment("myDocument.pdf", new ClassPathResource("uploads/document.pdf"));
+                
+                System.out.println("Mail sent hopefully");
+            }
+        };
+    }
+     */
+ 
 }
