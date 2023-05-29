de09ed63eec9a2ffc68133b0a3fe40e85d7df2f9
naz <naz.sevim@ug.bilkent.edu.tr>
2023-05-29 12:40:48 +0300

    Import excel file for student and evaluator

--- Backend/src/main/java/com/bilport/demo/controller/SuperadminController.java
+++ Backend/src/main/java/com/bilport/demo/controller/SuperadminController.java
@@ -1,13 +1,27 @@
 package com.bilport.demo.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bilport.demo.domain.model.Superadmin;
 import com.bilport.demo.domain.model.Admin;
@@ -25,9 +39,13 @@ import com.bilport.demo.service.EvaluatorService;
 public class SuperadminController {
     @Autowired
     SuperadminService superadminService;
+    @Autowired
     AdminService adminService;
+    @Autowired
     StudentService studentService;
+    @Autowired
     TAService taService;
+    @Autowired
     EvaluatorService evaluatorService;
 
     @ResponseBody
@@ -56,5 +74,114 @@ public class SuperadminController {
     // public List<Evaluator> getAllTAs(@PathVariable("id") String id) {
     public List<Evaluator> getAllEvaluators() {
         return evaluatorService.getEvaluators();
+    }
+
+    @PostMapping(value = "/studentImport")
+    public ResponseEntity<String> uploadExcelFileStudent(@RequestParam("file") MultipartFile file) {
+        try {
+            Workbook workbook = WorkbookFactory.create(file.getInputStream());
+
+            for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+                Sheet sheet = workbook.getSheetAt(i);
+                for (Row row : sheet) {
+                    // Assuming data starts from the second row
+                    if (row.getRowNum() > 0) {
+                        if (row.getCell(0).getStringCellValue().isEmpty() 
+                        || row.getCell(1).getStringCellValue().isEmpty()
+                        || row.getCell(2).getStringCellValue().isEmpty()
+                        || row.getCell(3).getStringCellValue().isEmpty()
+                        || row.getCell(4).getStringCellValue().isEmpty()) {
+                            return new ResponseEntity<>("An empty cell is found, check the file", HttpStatus.OK);
+                        }
+                        Student student = new Student();
+                        student.setUserName(row.getCell(0).getStringCellValue());
+                        student.setUserPassword(row.getCell(1).getStringCellValue());
+                        student.setUserAuthorities(new ArrayList<GrantedAuthority>() {{
+                            add(new SimpleGrantedAuthority("ROLE_STUDENT"));
+                        }});
+                        student.setStudentName(row.getCell(2).getStringCellValue());
+                        student.setStudentSurname(row.getCell(3).getStringCellValue());
+                        student.setStudentEmail(row.getCell(4).getStringCellValue());
+                        student.setAssignedEvaluatorId("");
+                        student.setAssignedSupervisorId("");
+                        student.setReports299(null);
+                        student.setReports399(null);
+
+                        
+                        if (row.getCell(5).getStringCellValue().length() == 11) {
+                            String[] takenCourses = new String[2];
+                            takenCourses[0] = row.getCell(5).getStringCellValue().substring(0, 5);
+                            takenCourses[1] = row.getCell(5).getStringCellValue().substring(6);
+                            student.setCoursesTaken(takenCourses);
+                        }
+                        else if (row.getCell(5).getStringCellValue().length() == 5) {
+                            String[] takenCourses = new String[1];
+                            takenCourses[0] = row.getCell(5).getStringCellValue().substring(0);
+                            student.setCoursesTaken(takenCourses);
+                        }
+                        
+                        studentService.createStudent(student);
+                    }
+                }
+            }
+            return new ResponseEntity<>("Successful Import", HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: Failed to read the file", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (EncryptedDocumentException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: Encrypted document found", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: Failed to import data", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping(value = "/evaluatorImport")
+    public ResponseEntity<String> uploadExcelFileEvaluator(@RequestParam("file") MultipartFile file) {
+        try {
+            Workbook workbook = WorkbookFactory.create(file.getInputStream());
+
+            for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+                Sheet sheet = workbook.getSheetAt(i);
+                for (Row row : sheet) {
+                    // Assuming data starts from the second row
+                    if (row.getRowNum() > 0) {
+                        if (row.getCell(0).getStringCellValue().isEmpty() 
+                        || row.getCell(1).getStringCellValue().isEmpty()
+                        || row.getCell(2).getStringCellValue().isEmpty()
+                        || row.getCell(3).getStringCellValue().isEmpty()
+                        || row.getCell(4).getStringCellValue().isEmpty()) {
+                            return new ResponseEntity<>("An empty cell is found, check the file", HttpStatus.OK);
+                        }
+                        Evaluator evaluator = new Evaluator();
+                        evaluator.setUserName(row.getCell(0).getStringCellValue());
+                        evaluator.setUserPassword(row.getCell(1).getStringCellValue());
+                        evaluator.setUserAuthorities(new ArrayList<GrantedAuthority>() {{
+                            add(new SimpleGrantedAuthority("ROLE_EVALUATOR"));
+                        }});
+                        evaluator.setAssignedStudents(null);
+                        evaluator.setEvaluatorName(row.getCell(2).getStringCellValue());
+                        evaluator.setEvaluatorSurname(row.getCell(3).getStringCellValue());
+                        evaluator.setEvaluatorEmail(row.getCell(4).getStringCellValue());
+                        evaluator.setAssignedStudents(null);
+                        evaluator.setStudentLimit((int) (row.getCell(5).getNumericCellValue()));
+
+                        evaluatorService.createEvaluator(evaluator);
+                    }
+                }
+            }
+
+            return new ResponseEntity<>("Successful Import", HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: Failed to read the file", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (EncryptedDocumentException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: Encrypted document found", HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error: Failed to import data", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- Frontend/bilport-webapp/src/components/superAdminComponents/SuperAdminStudents.jsx
+++ Frontend/bilport-webapp/src/components/superAdminComponents/SuperAdminStudents.jsx
@@ -1,12 +1,72 @@
 import StudentsPage from "../commonComponents/StudentsPage";
-import SuperAdminImportCard from "./SuperAdminImportCard";
+import React, { useState, useEffect } from 'react';
+import { Form, Card, Button, Stack } from 'react-bootstrap';
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 export default function SuperAdminStudents(props){
-    return(
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [uploadStatus, setUploadStatus] = useState(null);
+    
+    const axiosPrivate = useAxiosPrivate();
+    
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedFile(file);
+    };
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        if (selectedFile) {
+          const formData = new FormData();
+          formData.append('file', selectedFile);
+          console.log('a');
+      
+          
+          try {
+              const response = await axiosPrivate.post('/superadmins/studentImport',
+                  formData,
+                  {
+                      headers: { "Content-Type": "multipart/form-data" },
+                  }
+              );
+              setUploadStatus(response.data);
+              console.log('of of komur gibi yanıyorum');
+          } catch (err) {
+              console.error(err);
+          }
+    
+        }
+    }; 
+
+    return (
+        <div className="standaloneCard">
+        <Card style={{ color: '#008000' }}>
+          <Card.Body>
+            <Form.Group>
+              <Form.Label>Import {props.importUserType} from Excel spreadsheet:</Form.Label>
+              <Stack direction="horizontal" gap={3}>
+                <Form.Control type="file" accept=".xlsx, .xls" onChange={handleFileChange} />
+                <hr />
+                <Button variant="success" onClick={handleSubmit}>
+                  Import
+                </Button>
+              </Stack>
+              </Form.Group>
+                {uploadStatus && (
+                <p> {typeof uploadStatus === 'string' ? uploadStatus : 'Failed to import data'}</p>
+              )}
+            </Card.Body>
+          </Card>
+          <br/>
+            <StudentsPage students = {props.students} userType = "superadmin"/>
+        </div>
+      );
+    /*return(
         <div>
             <SuperAdminImportCard importUserType = "students"/>
             <br/>
             <StudentsPage students = {props.students} userType = "superadmin"/>
         </div>
-    )
+    )*/
 }
